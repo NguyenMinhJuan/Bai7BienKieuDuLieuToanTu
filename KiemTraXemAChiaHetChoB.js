a19d1f16127bd95cc620cd5b53205f8f3d539bfa
NguyenMinhJuan <nmtuan.dhti14a4hn@sv.uneti.edu.vn>
2024-06-11 10:38:04 +0700

    Nop bai tap

---
+++ KiemTraXemAChiaHetChoB.js
@@ -0,0 +1,10 @@
+let a=prompt("Moi ban nhap vao so thu A");
+let b=prompt("Moi ban nhap vao so thu B");
+if(a%b==0)
+{
+    alert("A chia het cho B");
+}
+else {
+    alert("A ko chia het cho B");
+}
+
